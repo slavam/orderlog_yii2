aeac98706f224db5767f74384f26ea33525478aa
s.shupanov <s.shupanov@vbr-bank.com.ua>
2012-07-20 15:28:35 +0300

    BLIN DOSTAL etot GIT and CHEREPAHA vmeste s nim!

--- protected/config/main.php
+++ protected/config/main.php
@@ -34,11 +34,10 @@ return array(
         'urlManager'=>array(
 		'urlFormat'=>'path',
 		'rules'=>array(
-        		'post/<id:\d+>/<title:.*?>'=>'post/view',
-        		'posts/<tag:.*?>'=>'post/index',
         		'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
         	),
         ),
+
 
         // uncomment the following to enable URLs in path-format
         /*
