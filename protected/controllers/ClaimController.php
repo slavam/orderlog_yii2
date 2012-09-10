4fde10d1c0c29701770e9a10c6d9b4f8c9bc65de
kriuchkov <v.kriuchkov@vbr-bank.com.ua>
2012-09-10 13:05:09 +0300

    fixed claim delete

--- protected/controllers/ClaimController.php
+++ protected/controllers/ClaimController.php
@@ -128,15 +128,22 @@ class ClaimController extends Controller
 	{
 		if(Yii::app()->request->isPostRequest)
 		{
-                    $lines = $this->loadModel($id)->claimLines;
-                    foreach ($lines as $line) {
-                        $line->delete();
+                    $model=$this->loadModel($id);
+                    if ($model->state->id ==1)
+                    {
+                        if (($model->delete()))
+                        {
+                            echo CJSON::encode(array('state'=>'ok','responce'=>'Удалена Заявка № '.$model->claim_number));
+                        }
+                        else echo CJSON::encode(array('state'=>'error','responce'=>'Ошибка удаления'));
+    //                                        $this->loadModel($id)->claimLines;
+    //                    foreach ($lines as $line) {
+    //                        $line->delete();
+    //                    }
+    //			 we only allow deletion via POST request
+    //			$this->loadModel($id)->delete();
                     }
-
-			// we only allow deletion via POST request
-			$this->loadModel($id)->delete();
-
-
+                    else {echo CJSON::encode(array('state'=>'error','responce'=>'Заявка не в статусе "Черновик". Удаление не возможно'));}
                         
 			// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
 //			if(!isset($_GET['ajax']))
@@ -149,7 +156,6 @@ class ClaimController extends Controller
 		else
 			throw new CHttpException(400,'Invalid request. Please do not repeat this request again.');
 	}
-
 	/**
 	 * Lists all models.
 	 */
