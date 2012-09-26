262a431c5854e363044a7f7330e6e23e38936718
s.shupanov <s.shupanov@vbr-bank.com.ua>
2012-09-26 12:25:38 +0300

    Changes in error modal window.

--- protected/views/asset/index.php
+++ protected/views/asset/index.php
@@ -321,20 +321,30 @@ top_bottom_pager_ButtonAdd ({
                     height:100,
                     buttons:{
                         'Да': function(){
-                            
+/*                            
                             var error_str = document.getElementById("error_del_dialog").innerHTML;
                             var old_error_str = error_str;
                             var tagList = error_str.split(',');
                             document.getElementById("error_del_dialog").innerHTML = tagList[0]+" "+
                                                                                     name_+","+
                                                                                     tagList[1];
+*/
                             var options = { 
                                 url: '<?php  echo Yii::app()->createUrl('asset/delete',array('id'=>''))?>'+id_,
                                 type: 'post',
                                 dataType: 'json',
                                 error: function(res, status, exeption) {
                                     
-//                                alert('Error: ' + res.responseCode+'  '+status+'  '+exeption);
+                                var tagList = res.responseText.split('.');
+                                
+                                document.getElementById("error_del_dialog").innerHTML = tagList[0]+": "+
+                                                                                    name_+"."+
+                                                                                    tagList[1];
+
+//                                document.getElementById("error_del_dialog").innerHTML = res.responseText;
+                                    
+//                                alert('Error: ' + res.responseCode+'  '+status+'  '+exeption+'  '+res.responseText);
+                                
                                 $("#error_del_dialog").dialog({
                                     title: 'Ошибка удаления!',
                                     modal:true,
@@ -342,7 +352,7 @@ top_bottom_pager_ButtonAdd ({
                                     height:150,
                                     buttons:{
                                         'Ok': function(){
-                                             document.getElementById("error_del_dialog").innerHTML = old_error_str;
+//                                             document.getElementById("error_del_dialog").innerHTML = old_error_str;
                                              $(this).dialog('close')
                                         }
                                        }
